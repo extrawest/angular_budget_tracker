dc08ef49e04cd29f219670f67a1aa945985cd9d8
Andrii Kapustin <andriikapustin@Andriis-MacBook-Pro.local>
2022-09-23 21:00:30 +0200

    firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAfM1DRDHN718HrXaNExeXRd7056ehrysI',
+    authDomain: 'budget-tracker-c1791.firebaseapp.com',
+    projectId: 'budget-tracker-c1791',
+    storageBucket: 'budget-tracker-c1791.appspot.com',
+    messagingSenderId: '811395747144',
+    appId: '1:811395747144:web:d43870c351e4d07cf8377d',
+    measurementId: 'G-ZVJYSMETK4'
+  },
 };
 
 /*
